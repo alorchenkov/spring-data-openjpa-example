62e93ea2b650a406942cc49168fbf596048b51fd
alorchenkov <a.lorchenkov@gmail.com>
2016-04-18 02:00:48 -0400

    Fixing Cloud CI build.

---
+++ ws-simulator-core/src/test/java/com/cpwr/gdo/simulator/rest/VehicleServiceTest.java
@@ -0,0 +1,14 @@
+package com.cpwr.gdo.simulator.rest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class VehicleServiceTest {
+    @Test
+    public void getVehicleByVin() throws Exception {
+
+    }
+
+}
